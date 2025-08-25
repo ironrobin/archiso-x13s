1e280501fc447067c3c95fd891f9d6467f3c48fd
Alex R <alex@ironrobin.net>
2025-08-25 13:22:33 -0500

    add support for thinkpad t14s oled edition

---
+++ configs/t14s-oled/profiledef.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archlinuxarm-t14s-oled"
+iso_label="ARCH_AARCH64_$(date +%Y%m)"
+iso_publisher="Alex R"
+iso_application="ARM64 Arch Linux Live/Rescue Disk for ThinkPad T14s Oled Edition"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('uefi-aarch64.systemd-boot.esp')
+arch="aarch64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'arm' '-b' '1M' '-Xdict-size' '1M')
+#airootfs_image_tool_options=('-zlz4hc,12')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/ironrobin-setup"]="0:0:755"
+  ["/usr/local/bin/workman"]="0:0:755"
+)
